5d96e89c8afffa34b2074b507c956da49ab07f2e
gilvan <gilvananjos10ns@gmail.com>
2025-12-07 17:48:11 -0300

    primeiro commit

---
+++ divulgacao_servicos/src/main/java/projeto/divulgacao_servicos/DivulgacaoServicosApplication.java
@@ -0,0 +1,13 @@
+package projeto.divulgacao_servicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivulgacaoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivulgacaoServicosApplication.class, args);
+	}
+
+}
---
+++ divulgacao_servicos/src/test/java/projeto/divulgacao_servicos/DivulgacaoServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.divulgacao_servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivulgacaoServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
